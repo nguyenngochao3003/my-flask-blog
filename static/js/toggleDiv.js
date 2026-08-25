d94b9ffdb1b733bffecbbdfc6556d530fc13d28f
nguyenngochao3003 <nguyenngochao3003@gmail.com>
2026-08-25 16:55:42 +0700

    test product.html

--- static/js/toggleDiv.js
+++
@@ -1,8 +0,0 @@
-function openForm(id) {
-  var element = document.getElementById(id);
-  element.style.display = 'block';
-}
-function closeForm(id) {
-  var element = document.getElementById(id);
-  element.style.display = 'none';
-}
